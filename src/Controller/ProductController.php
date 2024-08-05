e1d1c5f213440533bd529f6e99a78f371e798842
SadamOclock <sadam.siby@oclock.school>
2024-08-05 17:03:21 +0200

    upload image

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,12 +6,15 @@ use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
-#[Route('/product')]
+#[Route('/editor
+/product')]
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'app_product_index', methods: ['GET'])]
@@ -23,13 +26,30 @@ class ProductController extends AbstractController
     }
 
     #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('image')->getData();
+
+            if ($image){
+                $orginalName = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFileName = $slugger->slug($orginalName);
+                
+                $newFileName = $safeFileName.'-'.uniqid().'.'.$image->guessExtension();
+                try{
+                    $image->move(
+                        $this->getParameter('image_dir'),
+                        $newFileName
+
+                    );
+                }catch(FileException $exception){};
+
+                $product->setImage($newFileName);
+            }
             $entityManager->persist($product);
             $entityManager->flush();
 
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,10 +4,13 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\SubCategory;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class ProductType extends AbstractType
 {
@@ -17,6 +20,25 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
+            ->add('image', FileType::class,[
+                'label' => 'image de produit',
+                'mapped'=>false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        "maxSize"=> "1024k",
+                        "mimeTypes"=>[
+                            'image/jpg',
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        "maxSizeMessage"=> "Votre image ne doit pas deppasez les 1024",
+                        "mimeTypesMessage"=> "Votre image doit être de format valide (Jpg, png, jepg)"
+                    ])
+                ]
+                    
+                
+            ])
             ->add('subCategories', EntityType::class, [
                 'class' => SubCategory::class,
                 'choice_label' => 'name',
