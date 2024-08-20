e392fc86c3c3424fd282915696721805df82d1f8
SadamOclock <sadam.siby@oclock.school>
2024-08-20 22:12:26 +0200

    products display and product page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods:['GET'])]
+    public function index(ProductRepository $productRepository): Response
     {
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $productRepository->findBy([], ['id'=>"DESC"])
+        ]);
+    }
+
+    #[Route('/product/{id}/show', name: 'app_home_product_show', methods:['GET'])]
+    public function show(Product $product, ProductRepository $productRepository): Response
+    {
+       $lastProducts = $productRepository->findBy([], ['id'=>'DESC'], 5);
+
+        return $this->render('home/show.html.twig', [
+            'product' => $product,
+            'products' => $lastProducts
         ]);
     }
 }
